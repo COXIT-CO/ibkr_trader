9db86ee4718f75646114760e9d9e4efb06525916
sviddo <snisarvlad04@gmail.com>
2022-12-11 16:25:57 +0200

    feat: update intermediate server

--- scrape_server/src/__init__.py
+++ scrape_server/src/__init__.py
@@ -1,5 +1,5 @@
 import yaml
-import os
+import time
 from . import validators
 from . import helpers
 
@@ -9,12 +9,21 @@ except ImportError:
     # Handle Python 2.x
     import SimpleHTTPServer as server
 
+balance_check_result = None
+
 class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
     """Extend SimpleHTTPRequestHandler to handle POST requests"""
     def do_POST(self):
         """Save a file following a HTTP POST request"""
         file_length = int(self.headers['Content-Length'])
-        
+        if self.path == "/balance_check":
+            global balance_check_result
+            balance_check_result = self.rfile.read(file_length).decode()
+            response_tuple = response_tuple = (200, 'Ok')
+            self.send_response(*response_tuple)
+            self.end_headers()
+            self.wfile.write(response.encode('utf-8'))
+            return
         try:
             stock_data = yaml.full_load(self.rfile.read(file_length))
             probable_error = validators.validate_yaml_data(stock_data)
@@ -27,16 +36,27 @@ See pattern in samples folder in project root"""
             response = """File was accepted by intermediate server
 Checking availability to buy stocks ...\n"""
             response_tuple = (201, 'Created')
+            self.send_response(*response_tuple)
+            self.end_headers()
             helpers.generate_yaml_file(
                 helpers.beautify_stock_data(stock_data),
                 "config.yaml",
             )
+            self.wfile.write(response.encode('utf-8'))
+            for _ in range(30):
+                if balance_check_result is not None:
+                    self.wfile.write(balance_check_result.encode('utf-8'))
+                    return
+                time.sleep(1)
+            second_response = """Now market is closed or some stocks aren't being sold,
+so total order cost can't be estimated. Try some time later"""
+            self.wfile.write(second_response.encode('utf-8'))
         else:
             response = probable_error + "\n"
             response_tuple = (400, 'Rejected')
-        self.send_response(*response_tuple)
-        self.end_headers()
-        self.wfile.write(response.encode('utf-8'))
+            self.send_response(*response_tuple)
+            self.end_headers()
+            self.wfile.write(response.encode('utf-8'))
 
 
 
